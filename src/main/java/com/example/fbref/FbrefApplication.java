3b0e57d25f41d00fe96e1f38c7bcacf9cdcfeaaf
A.Faruk PALA <ahmetfaruk.pala@kolaysoft.com.tr>
2025-10-04 12:10:03 +0300

    başlangıç commit

---
+++ src/main/java/com/example/fbref/FbrefApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbrefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbrefApplication.class, args);
+	}
+
+}
